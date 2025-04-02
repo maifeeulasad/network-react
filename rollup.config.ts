f74b5209bf3aa56284f92444a15d3551766a3e36
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-04-02 20:08:00 +0600

    [rollup,build]: defined build configuration;

---
+++ rollup.config.ts
@@ -0,0 +1,42 @@
+// rollup.config.ts
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import packageJson from "./package.json" 
+import type { RollupOptions } from "rollup";
+
+const config: RollupOptions[] = [
+    {
+        input: "./network.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+            terser(),
+        ],
+        external: ["react"],
+    },
+    {
+        input: "./network.ts",
+        output: [{ file: "dist/types.d.ts", format: "es" }],
+        plugins: [dts()],
+    },
+];
+
+export default config;
