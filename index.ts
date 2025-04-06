724c2cb220eda7454a88ed7daf2cf00a5682cbcf
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-04-06 15:51:00 +0600

    [build]: combine both `fetch` and `graphql` into one;

---
+++ index.ts
@@ -0,0 +1,30 @@
+import {
+    useFetch,
+    useFetchGet,
+    useFetchHead,
+    useFetchPost,
+    useFetchPut,
+    useFetchDelete,
+    useFetchConnect,
+    useFetchOptions,
+    useFetchTrace,
+    useFetchPatch,
+} from './useFetch';
+
+import { useGraphQL } from './useGraphQL';
+
+export {
+    // fetch
+    useFetch,
+    useFetchGet,
+    useFetchHead,
+    useFetchPost,
+    useFetchPut,
+    useFetchDelete,
+    useFetchConnect,
+    useFetchOptions,
+    useFetchTrace,
+    useFetchPatch,
+    // graphql
+    useGraphQL
+};
